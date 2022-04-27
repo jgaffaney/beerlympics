07c98fa7cf016f9510065a5049b09a80b09539df
John Gaffaney <john.gaffaney@icloud.com>
2022-04-27 10:25:50 -0500

    fixed duplicate copyright

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.css';
 
 function Footer() {
-  return <footer>&copy; &copy;John Gaffaney</footer>;
+  return <footer>&copy; John Gaffaney</footer>;
 }
 
 export default Footer;
